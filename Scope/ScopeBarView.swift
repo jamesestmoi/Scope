5a8bfa128624e6d8f65a66063689bbfacfc246da
James Pickering <jllpickering@gmail.com>
2015-06-13 12:58:27 -0400

    Switching back to Yosemite

---
+++ Scope/ScopeBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ScopeBarView.swift
+//  
+//
+//  Created by James Pickering on 6/13/15.
+//
+//
+
+import UIKit
+
+class ScopeBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
